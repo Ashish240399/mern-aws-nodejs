14d470fcd97920a168621b3c2b3c20e41af0924a
Ashish Kumar Mohanty <ashishkumarmohanty7797@gmail.com>
2024-05-19 14:04:40 +0530

    add sanjita name

--- server/src/controllers/hello.js
+++ server/src/controllers/hello.js
@@ -1,5 +1,5 @@
 const hello = (req, res) => {
-    return res.json({message:"Hello from server Ashish after changing port with modification of --init!"});
+    return res.json({message:"Hello from Sanjita maharana!"});
 }
 
 module.exports = {hello};
